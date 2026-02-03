9c5ffa93e666b8122dfbb42bda996891753ea072
Joe Badger <badger.joseph@gmail.com>
2026-02-02 19:34:50 -0600

    fix: correct smoke test import in publish workflow

--- src/schemaform/__init__.py
+++ src/schemaform/__init__.py
@@ -1,4 +1,17 @@
+from importlib.metadata import PackageNotFoundError, version
+
 from .forms import SchemaForm
 from .types import FileUpload, ImageUpload, UploadedFileWrapper
 
-__all__ = ["SchemaForm", "FileUpload", "ImageUpload", "UploadedFileWrapper"]
+try:
+    __version__ = version("django-schemaform")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
+
+__all__ = [
+    "SchemaForm",
+    "FileUpload",
+    "ImageUpload",
+    "UploadedFileWrapper",
+    "__version__",
+]
